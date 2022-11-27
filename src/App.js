e0d491e6eb02e5fedc79234193c91b376638cd65
Leonan <leonanm04@gmail.com>
2022-11-27 15:42:57 -0300

    remoção variavel desnecessária

--- src/App.js
+++ src/App.js
@@ -13,24 +13,21 @@ function App() {
   const [word, setWord] = useState(words[Math.floor(Math.random() * words.length)]);
   const [runing, setRuning] = useState(0);
   const [errors, setErrors] = useState(0);
-  const [letter, setLetter] = useState("");
+  const [done, setDone] = useState(0)
 
   const dashWord = Array(0).fill("")
   const [dashArray, setdashArray] = useState(dashWord);
-
-  const [done, setDone] = useState(0)
 
   return (
     <div className="app">
       <Jogo runing={runing} setRuning={setRuning}
         word={word} setWord={setWord}
         errors={errors} done={done}
-        letter={letter} words={words} dashArray={dashArray} setdashArray={setdashArray} />
+        words={words} dashArray={dashArray} setdashArray={setdashArray} />
 
       <div className="bottom">
         <Letras word={word} setDone={setDone}
           runing={runing} setRuning={setRuning}
-          letter={letter} setLetter={setLetter}
           errors={errors} setErrors={setErrors}
           dashArray={dashArray} setdashArray={setdashArray} />
         <Chute runing={runing} setRuning={setRuning}
